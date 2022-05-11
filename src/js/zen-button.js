8658c4cd51a51bcc6776d6094eb452d07fc8c1db
ux-ui-pro <maxim@ux-ui.pro>
2022-05-11 05:46:28 +0300

    release 1.0.3

--- src/js/zen-button.js
+++ src/js/zen-button.js
@@ -1,34 +1,5 @@
-export default function Ripple() {
+import rippleButton from "./modules/ripple.js"
 
-    function createRipple(event) {
-        let button = event.currentTarget
+rippleButton()
 
-        let circle = document.createElement("span")
-        let diameter = Math.max(button.clientWidth, button.clientHeight)
-        let radius = diameter / 2
 
-        let vpOffset = button.getBoundingClientRect()
-
-        circle.style.width = circle.style.height = `${diameter}px`
-        circle.style.left = `${event.pageX - scrollX - (vpOffset.left + radius)}px`
-        circle.style.top = `${event.pageY - scrollY - (vpOffset.top + radius)}px`
-        circle.classList.add("ripple")
-
-        let ripple = button.querySelectorAll(".ripple")[0]
-
-        if (ripple) {
-            ripple.remove()
-        }
-
-        button.prepend(circle)
-    }
-
-    let buttons = document.querySelectorAll(".btn--ripple")
-
-    for (let button of buttons) {
-        button.addEventListener("click", createRipple)
-    }
-
-}
-
-Ripple()
