df0dd75d7054df72867d06225d8d6a98354eb3b1
ux-ui-pro <51360899+ux-ui-pro@users.noreply.github.com>
2022-05-15 23:23:12 +0300

    release 1.0.12

--- src/js/modules/ripple-button.js
+++ src/js/modules/ripple-button.js
@@ -1,7 +1,7 @@
 function createRipple(event) {
     let button = event.currentTarget
 
-    let circle = document.createElement("span")
+    let circle = document.createElement('span')
     let diameter = Math.max(button.clientWidth, button.clientHeight)
     let radius = diameter / 2
 
@@ -10,22 +10,22 @@ function createRipple(event) {
     circle.style.width = circle.style.height = `${diameter}px`
     circle.style.left = `${event.pageX - scrollX - (vpOffset.left + radius)}px`
     circle.style.top = `${event.pageY - scrollY - (vpOffset.top + radius)}px`
-    circle.classList.add("ripple")
+    circle.classList.add('ripple')
 
-    let ripple = button.querySelectorAll(".ripple")[0]
+    let ripple = button.querySelectorAll('.ripple')[0]
 
     if (ripple) {
         ripple.remove()
     }
 
-    button.prepend(circle)
+    button.insertAdjacentElement('afterbegin', circle)
 }
 
 export default function rippleButton() {
-    const buttons = document.querySelectorAll(".btn--ripple")
+    let buttons = document.querySelectorAll('.btn--ripple')
 
     for (let button of buttons) {
-        button.addEventListener("click", createRipple)
+        button.addEventListener('click', createRipple)
     }
 }
 
